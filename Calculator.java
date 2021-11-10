c39362b595c496028d28c5445f19ae9687b2d188
IT19223322 <savindujr@gmail.com>
2021-11-10 05:50:20 +0530

    init

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+    public void add(int a,int b){
+        System.out.println("total :"+(a+b));
+    }
+
+    public void add(Double a,Double b){
+        System.out.println("total :"+ (a + b));
+    }
+
+    public void add(int a,int b ,int c){
+        System.out.println("total :"+ (a + b +c));
+    }
+
+    public void add(Double a,Double b ,Double c){
+        System.out.println("total :"+ (a + b +c));
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.add(5, 6);
+        c1.add(6.7, 8.9);
+        c1.add(3,4,5);
+        c1.add(3,4,5);
+    }
+}
